f2e7ae920c6ceb3a9292f4f3f8e20b10ce8ca905
karthikeyan <bkarthikeyan28@hotmail.com>
2015-11-25 14:35:00 +0530

    Included changes to Dialog

--- share.js
+++ share.js
@@ -1,11 +1,22 @@
 
+function createIFrame(xPos, yPos, selectedText) {
+	var dialogDiv = document.createElement('div');
+	dialogDiv.id = "dialog";
+	dialogDiv.style.cssText = "position: absolute; top:" + y + "px; left: " + x "px;";
+	console.log(dialogDiv);
+	var x = chrome.runtime.getURL('choose.html');
+	document.body.appendChild(dialogDiv);
+	$('#dialog').load(x);
+	$('#edit').html(selectedText);
+}
+
+
 document.addEventListener('mouseup', function getSelectedText(evt) {
-	var x = window.getSelection().toString();
+	var selectedText = window.getSelection().toString();
 	var xPos = evt.clientX;
 	var yPos = evt.clientY;
-	if(x.length) {
-		chrome.runtime.sendMessage(x, function(response) {
-			console.log("Sent Message " + xPos + " " + yPos + " Received: " + response.farewell);
-		});
-	}
+	console.log(selectedText);
+	createIFrame(xPos, yPos, selectedText);	
 });
+
+
